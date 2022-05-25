181570dd6e63e97b4fb1465d2eea9552282ac56f
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-05-25 10:02:43 +0200

    Add create 25 files

--- generate_files.sh
+++ generate_files.sh
@@ -4,3 +4,12 @@ current_date=`date +"%Y-%m-%d"`
 directory_name=$USER-$current_date
 echo "Create directory $directory_name"
 mkdir $directory_name
+
+counter=0
+
+while [ $counter -lt 25 ] 
+do
+    filecounter=$((counter + 1))
+    touch "$directory_name/$filecounter"
+    ((counter++))
+done
